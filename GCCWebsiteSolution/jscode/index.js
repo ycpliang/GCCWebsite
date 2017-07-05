eb6ed790036e801d8ba98f1215e182eb229ce2f6
ycpliang <ycpliang@uwaterloo.ca>
2017-07-05 19:16:36 -0400

    modify footer and stuff

--- GCCWebsiteSolution/jscode/index.js
+++ GCCWebsiteSolution/jscode/index.js
@@ -1,3 +1,13 @@
 ﻿$(document).ready(function () {
-    
+    var timeoutId = 0;
+    function clickFunction() {
+        
+    }
+
+
+    $("#gcc-logo-center").on('mousedown', function () {
+        timeoutId = setTimeout(clickFunction, 1000);
+    }).on('mouseup mouseleave', function () {
+        clearTimeout(timeoutId);
+    });
 });
